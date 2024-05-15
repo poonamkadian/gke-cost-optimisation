8c98d9d2024239fdbc8589fed9b2deb3d367de84
POONAMKADIAN <poonamkadian@IN-POONAMKADIAN.local>
2024-05-15 18:02:08 +0530

    Readme updated

--- cost-configure.sh
+++ cost-configure.sh
@@ -1,6 +1,4 @@
 gcloud container clusters update gke1 enable-resource-consumption-metering --zone us-central1-a  --resource-usage-bigquery-dataset gke-costing
 
-gcloud container clusters update gke1 --enable-cost-allocation
-
 gcloud container clusters update gke1 --resource-usage-bigquery-dataset gke-cost
 
